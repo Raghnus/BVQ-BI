4c373689dcf1ef6da5a2bed36f886bab54520059
JJátiva <43797990+Raghnus@users.noreply.github.com>
2026-09-23 21:01:59 -0500

    cargar archivo
    cargar archivo  linea 6

--- app.py
+++ app.py
@@ -2,3 +2,5 @@ import streamlit as st
 st.title("Bolsa de Valores Quito BI")
 st.sidebar.title("Parámetros")
 st.write("Elaborado por: Juan Játiva")
+
+archivo = st.file_uploader("Cargue su archivo")
